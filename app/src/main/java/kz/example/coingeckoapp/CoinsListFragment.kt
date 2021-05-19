94355f3b7e3acd3a3df129f7621b84a15461b7fa
Arslan Tsoy <tsoy.ar@rahmetapp.kz>
2021-05-19 21:08:49 +0600

    setup pagination

--- app/src/main/java/kz/example/coingeckoapp/CoinsListFragment.kt
+++ app/src/main/java/kz/example/coingeckoapp/CoinsListFragment.kt
@@ -5,24 +5,22 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.jaredsburrows.retrofit2.adapter.synchronous.SynchronousCallAdapterFactory
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import kz.example.coingeckoapp.models.CoinModel
 import kz.example.coingeckoapp.recycler_views.CoinListAdapter
 import kz.example.coingeckoapp.retrofit.CoinInterface
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 class CoinsListFragment : Fragment() {
@@ -32,9 +30,12 @@ class CoinsListFragment : Fragment() {
     private var retrofit: Retrofit? = null
     private var coinInterface: CoinInterface? = null
 
-    private var progress: ProgressBar? = null
+    private var swipeLayout: SwipeRefreshLayout? = null
 
     private val compositeDisposable = CompositeDisposable()
+
+    private var pageNumber: Int = 1
+    private var isLoading: Boolean = false
     //endregion
 
     //region Overridden Methods
@@ -48,10 +49,26 @@ class CoinsListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        progress = view.findViewById(R.id.progressBar)
-        progress?.visibility = View.VISIBLE
+        swipeLayout = view.findViewById(R.id.swipeRefreshLayout)
+        swipeLayout?.isRefreshing = true
+        swipeLayout?.setOnRefreshListener {
+            refresh()
+        }
         setupRV()
-        getListCoins(1)
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://api.coingecko.com/")
+            .client(
+                OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .build()
+            )
+            .addCallAdapterFactory(SynchronousCallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        coinInterface = retrofit?.create(CoinInterface::class.java)
+        getListCoins(pageNumber, false)
     }
 
     override fun onStop() {
@@ -63,22 +80,48 @@ class CoinsListFragment : Fragment() {
     //region Support
     private fun setupRV() {
         val rvCoins = view?.findViewById<RecyclerView>(R.id.rvCoinList)
-        rvCoins?.layoutManager = LinearLayoutManager(requireContext())
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        rvCoins?.layoutManager = linearLayoutManager
+        rvCoins?.addOnScrollListener(
+            object : RecyclerView.OnScrollListener() {
+
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    Log.i("myCoins", "onScrollStateChanged: $newState")
+                }
+
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    Log.i("myCoins", "onScrolled: dx: $dx, dy: $dy")
+                    val visibleItemCount = linearLayoutManager.childCount
+                    val totalItemCount = linearLayoutManager.itemCount
+                    val firstVisibleItemPosition = linearLayoutManager.findFirstVisibleItemPosition()
+
+                    if (visibleItemCount + firstVisibleItemPosition >= totalItemCount && firstVisibleItemPosition >= 0) {
+                        getListCoins(pageNumber, false)
+                    }
+                }
+            })
         rvCoins?.adapter = coinAdapter
-        retrofit = Retrofit.Builder()
-            .baseUrl("https://api.coingecko.com/")
-            .addCallAdapterFactory(SynchronousCallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        coinInterface = retrofit?.create(CoinInterface::class.java)
+
     }
 
-    private fun getListCoins(page: Int) {
+    private fun refresh() {
+        pageNumber = 1
+        getListCoins(pageNumber, true)
+    }
+
+    private fun getListCoins(page: Int, isRefresh: Boolean) {
+        if(isLoading) {
+            return
+        }
+        isLoading = true
+        swipeLayout?.isRefreshing = true
         val disposableGetCoinList = Single.fromCallable {
             coinInterface?.getCoinList(
                 "usd",
                 "market_cap_desc",
-                50,
+                20,
                 page,
                 false
             )
@@ -87,15 +130,22 @@ class CoinsListFragment : Fragment() {
             .subscribeOn(Schedulers.io())
             .subscribe(
                 { nullableCoins ->
+                    pageNumber++
+                    isLoading = false
                     nullableCoins?.let { nonNullableCoins ->
-                        coinAdapter.addItems(nonNullableCoins)
+                        if(isRefresh) {
+                            coinAdapter.refreshItems(nonNullableCoins)
+                        } else {
+                            coinAdapter.addItems(nonNullableCoins)
+                        }
                     }
-                    progress?.visibility = View.GONE
+                    swipeLayout?.isRefreshing = false
                 }, {
+                    isLoading = false
                     Log.i("myCoinsListFragment", "$it")
                     it.printStackTrace()
                     Toast.makeText(requireContext(), "Error: $it", Toast.LENGTH_SHORT).show()
-                    progress?.visibility = View.GONE
+                    swipeLayout?.isRefreshing = false
                 })
         compositeDisposable.add(disposableGetCoinList)
     }
